4f9072896aeaa68e6deb467b81de9bc7a13c2702
davidfurore <d.huson@furore.com>
2024-07-03 08:45:07 +0200

    remove comment

--- routes/api.php
+++ routes/api.php
@@ -4,19 +4,3 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MatchController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
-
-
-// Route::post('/login', [AuthController::class, 'login']);
-// Route::post('/register', [AuthController::class, 'register']);
-// Route::post('/logout', [AuthController::class, 'logout']);
-
-// // protected
-// Route::group(['middleware' => ["auth:sanctum"]], function () {
-//     Route::resource('/matches', MatchController::class);
-//     // Route::post('/logout', [AuthController::class, 'logout']);
-//     Route::get('/dashboard', []);
-// });
